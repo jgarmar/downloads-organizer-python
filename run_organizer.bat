dacc4c735f738673949bd6a7c15d47f337f5dd1e
Jaolmos <jose.a.olmosmartinez@gmail.com>
2024-12-06 16:05:43 +0100

    Reverted change in run_organizer.bat to restore CMD window visibility

--- run_organizer.bat
+++ run_organizer.bat
@@ -1,6 +1,5 @@
-:: Downloads Organizer - Background Process Launcher
-:: This script launches the downloads organizer using pythonw.exe (no console window)
-:: Uses the Python interpreter from the virtual environment
-
 @echo off
-start /B "" "%~dp0venv\Scripts\pythonw.exe" "%~dp0downloads_organizer.py"
+cd %~dp0
+call venv\Scripts\activate
+python downloads_organizer.py
+pause
